087e6aff05a8560f7b072c0e9a31b951824dc001
MairaCelestino <mairacelestino@hotmail.com>
2024-01-05 18:17:35 +0000

    created session and logout validation

--- cypress/e2e/gui/logout.cy.js
+++ cypress/e2e/gui/logout.cy.js
@@ -1,14 +1,13 @@
-const cypressConfig = require("../../../cypress.config")
-
 describe('Logout', () => {
     beforeEach(() => {
         cy.login()
         cy.visit('/')
     })
-})
 
-it('successfully', ()=> {
-    cy.logout()
 
-    cy.url().should('be.equal', `${Cypress.config('baseUrl')}/users/sign_in`)
+    it('successfully', () => {
+        cy.logout()
+
+        cy.url().should('be.equal', `${Cypress.config('baseUrl')}/users/sign_in`)
+    })
 })
