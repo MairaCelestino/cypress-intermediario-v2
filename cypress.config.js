0f70a5de9610dfb4b4dae3071a16cdfc08e4ad3f
MairaCelestino <mairacelestino@hotmail.com>
2024-01-05 00:52:35 +0000

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost',
+  },
+  fixturesFolder: false,
+  video: false,
+})
---
+++ cypress/e2e/gui/logout.cy.js
@@ -0,0 +1,14 @@
+const cypressConfig = require("../../../cypress.config")
+
+describe('Logout', () => {
+    beforeEach(() => {
+        cy.login()
+        cy.visit('/')
+    })
+})
+
+it('successfully', ()=> {
+    cy.logout()
+
+    cy.url().should('be.equal', `${Cypress.config('baseUrl')}/users/sign_in`)
+})
